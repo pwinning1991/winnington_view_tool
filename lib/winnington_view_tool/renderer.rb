af436d2b1a09a45258a7991b29c6d101a2065d8a
Philip Winnington <ftcpjw@yahoo.com>
2018-12-02 22:43:04 -0500

    updated to fix a bug

--- lib/winnington_view_tool/renderer.rb
+++ lib/winnington_view_tool/renderer.rb
@@ -1,6 +1,6 @@
 module WinningtonViewTool
   class Renderer
-    def self.copyright name.msg
+    def self.copyright name,msg
       "&copy: #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
     end
   end
--- lib/winnington_view_tool/version.rb
+++ lib/winnington_view_tool/version.rb
@@ -1,3 +1,3 @@
 module WinningtonViewTool
-  VERSION = "0.2.0"
+  VERSION = "0.3.0"
 end
